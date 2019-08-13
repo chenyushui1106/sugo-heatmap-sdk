0463f1a8f69d592d3f068bf4c05313cfaa78bac7
shui <chenyushui1106@163.com>
2019-08-13 11:22:14 +0800

    submit code

---
+++ SugoHeatMap/SugoHeatMap.h
@@ -0,0 +1,19 @@
+//
+//  SugoHeatMap.h
+//  SugoHeatMap
+//
+//  Created by 陈宇艺 on 2019/8/13.
+//  Copyright © 2019 sugoio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SugoHeatMap.
+FOUNDATION_EXPORT double SugoHeatMapVersionNumber;
+
+//! Project version string for SugoHeatMap.
+FOUNDATION_EXPORT const unsigned char SugoHeatMapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SugoHeatMap/PublicHeader.h>
+
+
